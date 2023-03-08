f88fded2ffe98c5fa0282e0ae74eeed01af0daf4
Graham Rowlands <grahamrow@gmail.com>
2023-03-08 16:21:46 -0500

    Add python requirements

--- setup.py
+++ setup.py
@@ -11,5 +11,11 @@ setup(
     url='https://github.com/BBN-Q/pyWRspice',
     license='MIT License',
     description='Python wrapper for WRspice circuit simulation',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy >= 1.12.1",
+        "scipy >= 0.17.1",
+        "pandas >= 1.3.5",
+        "paramiko >= 2.9.5"
+    ],
 )
